d860cf3a7693760b7c884c43f73bfe20dda07cda
praveen-jpeg <79192791+Praveen-jpeg@users.noreply.github.com>
2025-12-18 10:04:47 +0530

    Starting Chapter 2.

---
+++ Chapter 2/18.2/ex2.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main(){
+    // Using standard Headers
+    printf("signed char     :%d to %d\n",SCHAR_MIN,SCHAR_MAX);
+    printf("unsigned char   :0 to %d\n",UCHAR_MAX);
+    printf("signed short    :%d to %d\n",SHRT_MIN,SHRT_MAX);
+    printf("unsigned short  :0 to %d\n",USHRT_MAX);
+    printf("signed int      :%d to %d\n",INT_MIN,INT_MAX);
+    printf("unsigned int    :0 to %d\n",UINT_MAX);
+    
+
+}
