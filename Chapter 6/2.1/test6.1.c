e4c0a72d8ae6720773b1e0793c0a174dd0185b24
praveen-jpeg <79192791+Praveen-jpeg@users.noreply.github.com>
2026-01-02 16:32:23 +0530

    Added test files and output file sfor Exercises 6.1 to 6.4

---
+++ Chapter 6/2.1/test6.1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define MAX 100
+ int my_var = 10;
+ char *str = "Hello World";
+ char ch = '_';
+ /*This is a comment Line*/
+ int another_var;
+void sample(void){
+    int local =5;
+}
---
+++ Chapter 6/2.1/test6.2.c
@@ -0,0 +1,15 @@
+int counterValue;
+int counterVar;
+int counterTemp;
+int countExtra;
+
+int sampleValue;
+int sampleVar;
+
+char my_variable;
+char my_varCount;
+
+int main(){
+    int localCounter;
+    int localCount;
+}
